e5d7efa8b13d192ba448a1d45a776bb69c3a2302
sainath161 <sainathsmart1234@gmail.com>
2023-11-01 21:22:55 +0530

    youtube clone

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
-let API = "AIzaSyBtRQja8g3Rm7li_gzu36RRq4EibDqbBJs";
+let API = "AIzaSyCDpDBxYOiRNM0yDn68lWvGHj2GH1plLIw";
 
 async function mostPopular() {
-    let res = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=100&chart=mostPopular&regionCode=IN&key=${API}`);
+  try {
+    let res = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=2000&chart=mostPopular&regionCode=IN&key=${API}`);
+    if (!res.ok) {
+      throw new Error(`API request failed with status ${res.status}`);
+    }
     let data = await res.json();
     append(data.items);
-    // console.log(data.items);
+  } catch (error) {
+    console.error("Error fetching most popular videos:", error);
+  }
 }
-mostPopular();
-
-mostPopular();
 
 function append(data) {
   let container = document.getElementById("container");
+  container.innerHTML = ""; // Clear the container
+
   data.forEach(({ snippet, id: { videoId } }) => {
     let img = snippet.thumbnails.high.url;
     let title = snippet.title;
@@ -28,16 +33,88 @@ function append(data) {
     Cname.style.color = "white";
     detailsDiv.appendChild(name);
     detailsDiv.appendChild(Cname);
-    // let data = {
-    //     snippet,
-    //     videoId
-    // }
-    // div.addEventListener("click", function() {
-    //     localStorage.setItem("video", JSON.stringify(data));
-    //     window.location.href = 
-    // })
+
+    div.addEventListener("click", function () {
+      let data = {
+        snippet,
+        videoId
+      };
+      localStorage.setItem("video", JSON.stringify(data));
+      window.location.href = "videoDetails.html";
+    });
+
     div.appendChild(image);
     div.appendChild(detailsDiv); // Append the details container to the main div
     container.appendChild(div);
   });
 }
+
+async function search() {
+  let query = document.getElementById("query").value;
+  try {
+    let res = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1000&q=${query}&key=${API}`);
+    if (!res.ok) {
+      throw new Error(`API request failed with status ${res.status}`);
+    }
+    let data = await res.json();
+    append(data.items);
+  } catch (error) {
+    console.error("Error searching videos:", error);
+  }
+}
+
+// Function to trigger search on Enter key press
+function searchOnEnter(event) {
+  if (event.key === "Enter") {
+    search();
+  }
+}
+
+// Placeholder for voice search functionality
+function voiceSearch() {
+  const recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+
+  recognition.lang = 'en-US';
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+
+  recognition.start();
+
+  recognition.onresult = function (event) {
+    const voiceQuery = event.results[0][0].transcript;
+    document.getElementById("query").value = voiceQuery;
+    search();
+  };
+}
+
+mostPopular(); // Call the function to fetch and display most popular videos
+
+
+let sidebarOpen = true; // Start with the sidebar closed by default
+const sidebar = document.getElementById("sidebar");
+const container = document.getElementById("container");
+
+// Function to open or close the sidebar and adjust the container's grid-template-columns
+function toggleSidebar() {
+    sidebarOpen = !sidebarOpen;
+
+    if (sidebarOpen) {
+        sidebar.style.width = "17%";
+        container.style.marginLeft = "18.5%";
+        container.style.gridTemplateColumns = "repeat(3, 1fr)";
+    } else {
+        sidebar.style.width = "0";
+        container.style.marginLeft = "0";
+        container.style.gridTemplateColumns = "repeat(4, 1fr)";
+    }
+
+    // Adjust container's width to occupy the full screen when the sidebar is closed
+    if (!sidebarOpen) {
+        container.style.width = "100%";
+    } else {
+        container.style.width = "80%";
+    }
+}
+
+// Call the toggleSidebar function when the page loads to set the default container grid-template-columns
+toggleSidebar();
